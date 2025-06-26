bd48c4379675d1a977d821b444f08ec2d36eb9e1
menezesds <menezesds@hotmail.com>
2025-06-26 14:20:28 -0300

    Add the swagger to the project with an interface to try and two options to add the property information.

--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -1,16 +1,29 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 import { UniqueEmail } from "../validator/email.validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateUserDto {
     @IsNotEmpty({message: 'Name is required'})
+    @ApiProperty({
+        description: 'User Name',
+        example:'John Doe'
+    })
     name: string;
 
     @IsEmail(undefined, {message: 'Invalid email'})
     @IsNotEmpty({message: 'Email is required'})
     @UniqueEmail({message: 'Email already exists'})
+    @ApiProperty({
+        description: 'User Email',
+        example:'john.doe@example.com'
+    })
     email: string;
 
     @IsNotEmpty({message: 'Password is required'})
     @MinLength(6, {message: 'Password must be at least 6 characters long'})
+    @ApiProperty({
+        description: 'User Password',
+        example:'1B3c5d'
+    })
     password: string;
 }
